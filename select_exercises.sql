f06eb5cdf005e475a8e08a7b45b16f48279392fc
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-03-24 12:15:06 -0500

    adding comments to explain the code

--- select_exercises.sql
+++ select_exercises.sql
@@ -10,25 +10,28 @@ select database();
 describe albums;
 
 
--- a. How many rows are in the albums table?
--- 31 rows
+-- a. How many rows are in the albums table? (returns 31 rows)
+select *
+from albums;
 
--- b. How many unique artist names are in the albums table?
--- 23 rows
+-- 
+
+-- b. How many unique artist names are in the albums table? (returns 23 rows)
+
 select distinct artist
 from albums;
 
 -- c. What is the primary key for the albums table?
+describe albums;
 -- id
 
--- d. What is the oldest release date for any album in the albums table? What is the most recent release date?
-select release_date
+-- d. What is the oldest release date for any album in the albums table? (1967)
+-- What is the most recent release date?(2011)
+select distinct release_date
 from albums
 order by release_date; -- to sort in asc order
--- oldest release date is 1967
--- most recent release date is 2011
 
--- The name of all albums by Pink Floyd
+-- The name of all albums by Pink Floyd (returns 2 records)
 select name
 from albums
 where artist = 'Pink Floyd';
@@ -37,35 +40,37 @@ where artist = 'Pink Floyd';
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
 select * from albums; 
 
-select release_date 
+select name, release_date 
 from albums
-where name = "Sgt. Pepper's Lonely Hearts Club Band";
+where name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 -- year of release = 1967
 
 -- c. The genre for the album Nevermind
 select genre
 from albums
-where name = "Nevermind";
+where name = 'Nevermind';
 -- genre is Grunge, Alternative rock
 
--- d. Which albums were released in the 1990s
-select name 
+-- d. Which albums were released in the 1990s (returns 11 records)
+select name, release_date
 from albums
 where release_date between 1990 and 1999;
 -- 11 albums
 -- The Bodyguard, Jagged Little Pill, Come On Over, Falling into You, Let's Talk About Love, Dangerous, The Immaculate Collection, Titanic: Music from the Motion Picture, Metallica,Nevermind,Supernatural
 
 -- e. Which albums had less than 20 million certified sales
-select * from albums; 
-select name
+select name, sales
 from albums
 where sales < 20;
 -- 13 albums
 -- Grease: The Original Soundtrack from the Motion Picture, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love, Dangerous, The Immaculate Collection, Abbey Road,Born in the U.S.A., Brothers in Arms, Titanic: Music from the Motion Picture, Nevermind, The Wall
 
 -- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
-select name 
+select *
 from albums
-where genre = "Rock";
--- only returns rock genre because we are specifying that genre is equal to rock
+where genre = "Rock"; --  returns 5 records
+-- only returns rock genre because we are specifying that genre is equal to rock and progressive or pop rock is not an exact match
+
+select distinct genre
+from albums;
 
