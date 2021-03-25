7ba3466555b517d3d13faabe69d753c2c27782f6
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-03-25 17:22:43 -0500

    adding sql exercises for select statement

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,76 +1,76 @@
 -- Create a new file called select_exercises.sql
 
 -- Use the albums_db database
-use albums_db;
+USE albums_db;
 
 -- checking correct db is selected
-select database();
+SELECT DATABASE();
 
 -- Explore the structure of the albums table
-describe albums;
+DESCRIBE albums;
 
 
--- a. How many rows are in the albums table? (returns 31 rows)
-select *
-from albums;
+-- 1a. How many rows are in the albums table? 
+SELECT *
+FROM albums;
+-- returns 31 rows
 
--- 
+-- 1b. How many unique artist names are in the albums table? 
 
--- b. How many unique artist names are in the albums table? (returns 23 rows)
+SELECT DISTINCT artist
+FROM albums;
+-- returns 23 rows
 
-select distinct artist
-from albums;
+-- 1c. What is the primary key for the albums table?
+DESCRIBE albums;
+-- id is the primary key
 
--- c. What is the primary key for the albums table?
-describe albums;
--- id
+-- 1d. What is the oldest release date for any album in the albums table? 
+-- 1967
+-- What is the most recent release date?
+-- 2011
+SELECT DISTINCT release_date
+FROM albums
+ORDER BY release_date; -- to sort in asc order
 
--- d. What is the oldest release date for any album in the albums table? (1967)
--- What is the most recent release date?(2011)
-select distinct release_date
-from albums
-order by release_date; -- to sort in asc order
-
--- The name of all albums by Pink Floyd (returns 2 records)
-select name
-from albums
-where artist = 'Pink Floyd';
+-- The name of all albums by Pink Floyd 
+SELECT NAME
+FROM albums
+WHERE artist = 'Pink Floyd';
+-- returns 2 records
 -- album names: The Dark Side of the Moon, The Wall
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
-select * from albums; 
-
-select name, release_date 
-from albums
-where name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+SELECT NAME, release_date 
+FROM albums
+WHERE NAME = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 -- year of release = 1967
 
 -- c. The genre for the album Nevermind
-select genre
-from albums
-where name = 'Nevermind';
+SELECT genre
+FROM albums
+WHERE NAME = 'Nevermind';
 -- genre is Grunge, Alternative rock
 
--- d. Which albums were released in the 1990s (returns 11 records)
-select name, release_date
-from albums
-where release_date between 1990 and 1999;
--- 11 albums
+-- d. Which albums were released in the 1990s 
+SELECT NAME, release_date
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+-- returns 11 albums
 -- The Bodyguard, Jagged Little Pill, Come On Over, Falling into You, Let's Talk About Love, Dangerous, The Immaculate Collection, Titanic: Music from the Motion Picture, Metallica,Nevermind,Supernatural
 
 -- e. Which albums had less than 20 million certified sales
-select name, sales
-from albums
-where sales < 20;
+SELECT NAME, sales
+FROM albums
+WHERE sales < 20;
 -- 13 albums
 -- Grease: The Original Soundtrack from the Motion Picture, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love, Dangerous, The Immaculate Collection, Abbey Road,Born in the U.S.A., Brothers in Arms, Titanic: Music from the Motion Picture, Nevermind, The Wall
 
 -- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
-select *
-from albums
-where genre = "Rock"; --  returns 5 records
+SELECT *
+FROM albums
+WHERE genre = "Rock"; --  returns 5 records
 -- only returns rock genre because we are specifying that genre is equal to rock and progressive or pop rock is not an exact match
 
-select distinct genre
-from albums;
+
 
