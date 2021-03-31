8ea07600855b534e1d512bf2cef6c77032dab420
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-03-30 20:01:38 -0500

    updating to add tables from questions

--- join_exercises.sql
+++ join_exercises.sql
@@ -50,6 +50,19 @@ GROUP BY roles;
 -- from employees db
 
 -- 2. Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+
+/*  Department Name    | Department Manager
+ --------------------+--------------------
+  Customer Service   | Yuchang Weedman
+  Development        | Leon DasSarma
+  Finance            | Isamu Legleitner
+  Human Resources    | Karsten Sigstam
+  Marketing          | Vishwani Minakawa
+  Production         | Oscar Ghazalie
+  Quality Management | Dung Pesch
+  Research           | Hilary Kambil
+  Sales              | Hauke Zhang   */
+
 SELECT dept_name AS 'Department Name',
 CONCAT(first_name, ' ', e.last_name) AS 'Department Manager'
  FROM employees AS e
@@ -70,6 +83,14 @@ WHERE dept_manager.to_date LIKE '999%'
 ORDER BY dept_name;
 
 -- 3. Find the name of all departments currently managed by women.
+
+/* Department Name | Manager Name
+----------------+-----------------
+Development     | Leon DasSarma
+Finance         | Isamu Legleitner
+Human Resources | Karsetn Sigstam
+Research        | Hilary Kambil   */
+
 SELECT d.dept_name AS 'Department Name',
 CONCAT(first_name, ' ', last_name) AS 'Manager Name'
  FROM employees AS e
@@ -84,6 +105,17 @@ ORDER BY d.dept_name;
 
 
 -- 4. Find the current titles of employees currently working in the Customer Service department.
+
+/* Title              | Count
+-------------------+------
+Assistant Engineer |    68
+Engineer           |   627
+Manager            |     1
+Senior Engineer    |  1790
+Senior Staff       | 11268
+Staff              |  3574
+Technique Leader   |   241  */
+
 SELECT title, count(*)
 FROM titles t
 JOIN employees e ON t.emp_no = e.emp_no
@@ -92,8 +124,20 @@ WHERE de.dept_no = 'd009' AND t.to_date LIKE '9999%'
 GROUP BY title;
 
 
-
 -- 5. Find the current salary of all current managers.
+
+/* Department Name    | Name              | Salary
+-------------------+-------------------+-------
+Customer Service   | Yuchang Weedman   |  58745
+Development        | Leon DasSarma     |  74510
+Finance            | Isamu Legleitner  |  83457
+Human Resources    | Karsten Sigstam   |  65400
+Marketing          | Vishwani Minakawa | 106491
+Production         | Oscar Ghazalie    |  56654
+Quality Management | Dung Pesch        |  72876
+Research           | Hilary Kambil     |  79393
+Sales              | Hauke Zhang       | 101987   */
+
 SELECT dept_name AS 'Department Name',
 CONCAT(first_name, ' ', e.last_name) AS 'Department Manager', salary
  FROM employees AS e
@@ -108,8 +152,22 @@ AND s.to_date LIKE '9999%'
 ORDER BY dept_name;
 
 
-
 -- 6. Find the number of current employees in each department.
+
+/* +---------+--------------------+---------------+
+| dept_no | dept_name          | num_employees |
++---------+--------------------+---------------+
+| d001    | Marketing          | 14842         |
+| d002    | Finance            | 12437         |
+| d003    | Human Resources    | 12898         |
+| d004    | Production         | 53304         |
+| d005    | Development        | 61386         |
+| d006    | Quality Management | 14546         |
+| d007    | Sales              | 37701         |
+| d008    | Research           | 15441         |
+| d009    | Customer Service   | 17569         |
++---------+--------------------+---------------+  */
+
 SELECT de.dept_no, dept_name, count(e.emp_no) AS num_employees
 FROM dept_emp de
 JOIN departments d ON de.dept_no = d.dept_no
@@ -121,6 +179,12 @@ order by d.dept_no;
 
 
 -- 7. Which department has the highest average salary? Hint: Use current not historic information.
+
+/* +-----------+----------------+
+| dept_name | average_salary |
++-----------+----------------+
+| Sales     | 88852.9695     |
++-----------+----------------+  */
 
 SELECT dept_name, AVG(salary) AS average_salary
 FROM departments d
@@ -135,6 +199,12 @@ LIMIT 1;
 
 -- 8. Who is the highest paid employee in the Marketing department?
 
+/* +------------+-----------+
+| first_name | last_name |
++------------+-----------+
+| Akemi      | Warwick   |
++------------+-----------+  */
+
 SELECT first_name, last_name
 FROM employees e
 JOIN  salaries s ON s.emp_no = e.emp_no
@@ -146,6 +216,13 @@ LIMIT 1;
  
 
 -- 9. Which current department manager has the highest salary?
+
+/* +------------+-----------+--------+-----------+
+| first_name | last_name | salary | dept_name |
++------------+-----------+--------+-----------+
+| Vishwani   | Minakawa  | 106491 | Marketing |
++------------+-----------+--------+-----------+  */
+
 SELECT first_name, last_name, salary, dept_name
 FROM employees e
 JOIN salaries s ON s.emp_no = e.emp_no
