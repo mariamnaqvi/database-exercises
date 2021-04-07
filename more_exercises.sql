dc3268ae58e700c2a53eb08261b503ef2cd47919
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-04-06 21:41:31 -0500

    update code for exercise 1

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,6 +1,9 @@
 /* How much do the current managers of each department get paid, relative to the average salary for the department? 
 Is there any department where the department manager gets paid less than the average salary? */
 
+-- select the db
+USE employees;
+
 -- find current managers 
 SELECT emp_no, dept_no
 FROM dept_manager
@@ -14,31 +17,38 @@ AND s.to_date > now()
 WHERE dm.to_date > now();
 
 -- find average salary per department
-SELECT Round(AVG(salary),2) AS avg_sal,
-CASE 
-WHEN dept_no = 'd001' THEN 'd1_salary'
-WHEN dept_no = 'd002' THEN 'd2_salary'
-WHEN dept_no = 'd003' THEN 'd3_salary'
-WHEN dept_no = 'd004' THEN 'd4_salary'
-WHEN dept_no = 'd005' THEN 'd5_salary'
-WHEN dept_no = 'd006' THEN 'd6_salary'
-WHEN dept_no = 'd007' THEN 'd7_salary'
-WHEN dept_no = 'd008' THEN 'd8_salary'
-WHEN dept_no = 'd009' THEN 'd9_salary'
-ELSE 'other'
-END AS dept_no_salary
+SELECT Round(AVG(salary),2) AS avg_sal, dept_no
 FROM salaries s
 JOIN dept_emp de ON de.emp_no = s.emp_no
 AND de.to_date > now()
 WHERE s.to_date > now()
-GROUP BY dept_no_salary;
+GROUP BY dept_no;
 
--- Check current avg salary for d001 (returns 80058 same as above query)
-SELECT AVG(salary)
+-- find the current salary for current managers
+SELECT t1.dept_no, t1.manager_salary, t2.avg_salary FROM 
+(SELECT dm.emp_no, dept_no, salary AS manager_salary
+FROM dept_manager dm
+JOIN salaries s ON s.emp_no = dm.emp_no
+AND s.to_date > now()
+WHERE dm.to_date > now()) AS t1
+JOIN 
+(SELECT Round(AVG(salary),2) AS avg_salary, dept_no
 FROM salaries s
 JOIN dept_emp de ON de.emp_no = s.emp_no
 AND de.to_date > now()
-WHERE (dept_no = 'd001')
-AND s.to_date > now();
+WHERE s.to_date > now()
+GROUP BY dept_no) AS t2
+ON t1.dept_no = t2.dept_no;
 
--- create temp table that includes dept avg salary and dept manager's salary to compare
+/*
+"dept_no"|"manager_salary"|"avg_salary"
+"d001"   |106491          |80058.85
+"d002"   |83457           |78559.94
+"d003"   |65400           |63921.90
+"d004"   |56654           |67843.30
+"d005"   |74510           |67657.92
+"d006"   |72876           |65441.99
+"d007"   |101987          |88852.97
+"d008"   |79393           |67913.37
+"d009"   |58745           |67285.23
+*/
