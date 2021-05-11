6273d51c068f1269e1b4f6a96c904affd55ba4ae
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-05-11 08:54:20 -0500

    add ex

--- more_exercises.sql
+++ more_exercises.sql
@@ -710,3 +710,22 @@ SELECT payment_date, amount
 FROM payment
 ORDER BY amount DESC
 LIMIT 20;
+
+/*
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have 
+maximum total earnings. Then print these values as  space-separated integers.*/
+select (salary * months) as earnings ,count(*) 
+from employee 
+group by 1 
+order by earnings desc
+limit 1;
+
+-- d. Select the title, description, special features, length, and rental duration columns from the film table for the 
+-- first 10 films with behind the scenes footage under 2 hours in length and a rental duration between 5 and 7 days, 
+-- ordered by length in descending order.
+SELECT title, description, special_features, length, rental_duration
+FROM film 
+WHERE length < 120 
+AND `rental_duration` BETWEEN 5 AND 7
+ORDER BY length
+LIMIT 10;
